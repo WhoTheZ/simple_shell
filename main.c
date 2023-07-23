3097ff92a18c49415fe98e8a1560fc40faeead57
WhoTheZ <fizzkhalifa1211@gmail.com>
2023-07-22 17:24:16 -0700

    simpleShell

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * 
+ * main - init data
+ * @argc: input size if @argv
+ * @argv: input array of command line
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	data d;
+	(void)argc;
+	init_data(&d, argv[0];
+	_exec(&d);
+
+	return (0);
+
+	}
+			
