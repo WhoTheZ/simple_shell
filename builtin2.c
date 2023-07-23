9783af5f0f16560761c4af3d8c53a25cfcfe2ee7
WhoTheZ <fizzkhalifa1211@gmail.com>
2023-07-22 18:06:30 -0700

    builtin2.c

---
+++ builtin2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void builtin_cd(data *d)
+   {
+	   char *dir = d->av[1];
+	   char cwd[256];
+
+	   if (getcwd(cwd, sizeof(cwd)) == NULL)
+		   perror("getcwd() error");
+	   _setenv(d, "PWD", cwd);
+	   if(!dir)
+		   dir = _getenv("HOME");
+	   if (chdir(dir) == -1)
+		   perror("cd");
+	   else
+	   {
+		   _setnev(d, "OLDPWD", _getenv("PWD"));
+		   if (getcwd(cwd, sizeof(cwd)) = NULL)
+			   perror("getcwd() error");
+		   _setenv(d, "PWD", cwd);
+	   }
+   }
